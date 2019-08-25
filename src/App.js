676bdc9faa65b1399782a55d9aab6d2c7bc4bdc3
Asad0313 <asadaliwaliani@gmail.com>
2019-08-25 11:48:36 -0700

    mysecondcommit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App()  {
+
+   
+   var style = {
+ fontSize: 20,
+ color: 'red'
+
+   };
   return (
+   
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <h1 style={style}>Hello react World</h1>
+       
       </header>
     </div>
   );
+
+
 }
+
 
 export default App;
